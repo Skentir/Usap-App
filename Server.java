016445a04613c56a04c7ab765474e563e4c96b8c
unknown <nick.intatano@gmail.com>
2020-09-16 12:23:04 +0800

    added server functionalities

--- Server.java
+++ Server.java
@@ -1,86 +1,91 @@
 import java.io.*; 
 import java.net.*; 
-import java.util.Scanner; 
-
+import java.util.*; 
+  
+  
 // Server class 
-public class Server extends Thread 
+public class ServerTest extends Thread 
 { 
-     private static ServerSocket ss; 
-     private String nome; 
-     private Socket s;
-     private DataInputStream dis; 
-     private BufferedReader bfr;
-     // Store clients
-     private static ArrayList<BufferedWriter> clients;   
+	private static ArrayList<BufferedWriter> clients;           
+	private static ServerSocket server; 
+	private String nome;
+	private Socket con;
+	private InputStream in;  
+	private InputStreamReader inr;  
+	private BufferedReader bfr; 
 
     // Constructor
-    public Server (Socket s) {
-        this.s = s;
-        try {
-            dis = new DataInputStream(s.getInputStream());
-            bfr = new BufferedReader(dis); 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    public ServerTest(Socket con){
+		this.con = con;
+		try {
+			in  = con.getInputStream();
+			inr = new InputStreamReader(in);
+			bfr = new BufferedReader(inr);
+		} catch (IOException e) {
+			  e.printStackTrace();
+		}                          
+	}       
 
     // Run - Everytime a client connects, a new thread is allocated
     public void run() {
-        try {
-            String msg;
-            DataOutputStream dos = this.s.getOutputStream();
-            BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(dos));
-            
-            
-            clients.add(mtch); 
-            mtch.run();
+		try{								 
+			String msg;
+			OutputStream ou =  this.con.getOutputStream();
+			Writer ouw = new OutputStreamWriter(ou);
+			BufferedWriter bfw = new BufferedWriter(ouw); 
+			clients.add(bfw);
+			nome = msg = bfr.readLine();
+				  
+			while(!"Logout".equalsIgnoreCase(msg) && msg != null)
+			{           
+				msg = bfr.readLine();
+				sendToAll(bfw, msg);
+				System.out.println(msg);                                              
+			}										 
+		}catch (Exception e) {
+				e.printStackTrace();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		} 
     }
 
+	// sendToAll - when the server receives a message, it sends it to all other connected clients
+	public void sendToAll(BufferedWriter bwOutput, String msg) throws  IOException 
+	{
+		BufferedWriter bwS;
+
+		for(BufferedWriter bw :  clients){
+			bwS = (BufferedWriter)bw;
+			if(!(bwOutput == bwS)){
+				bw.write(nome + " -> " + msg+"\r\n");
+				bw.flush(); 
+			}
+		}          
+	}
+	
+	// main
     public static void main(String[] args) throws IOException  
     { 
-        // server is listening on port 1234 
-        ServerSocket ss = new ServerSocket(1234); 
-          
-        Socket s; 
-          
-        // running infinite loop for getting 
-        // client request 
-        while (true)  
-        { 
-            // Accept the incoming request 
-            s = ss.accept(); 
-  
-            System.out.println("New client request received : " + s); 
-              
-            // obtain input and output streams 
-            DataInputStream dis = new DataInputStream(s.getInputStream()); 
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-              
-            System.out.println("Creating a new handler for this client..."); 
-  
-            // Create a new handler object for handling this request. 
-            ClientHandler mtch = new ClientHandler(s,"client " + i, dis, dos); 
-  
-            // Create a new Thread with this object. 
-            Thread t = new Thread(mtch); 
-              
-            System.out.println("Adding this client to active client list"); 
-  
-            // add this client to active clients list 
-            ar.add(mtch); 
-  
-            // start the thread. 
-            t.start(); 
-  
-            // increment i for new client. 
-            // i is used for naming only, and can be replaced 
-            // by any naming scheme 
-            i++; 
-  
-        } 
+		try{
+			//Cria os objetos necessário para instânciar o server
+			/*JLabel lblMessage = new JLabel("Server Port:");
+			JTextField txtPort = new JTextField("12345");
+			Object[] texts = {lblMessage, txtPort };  
+			JOptionPane.showMessageDialog(null, texts);*/
+			server = new ServerSocket(Integer.parseInt(args[0]));
+			clients = new ArrayList<BufferedWriter>();
+			//JOptionPane.showMessageDialog(null,"Active Server at Port: "+ txtPort.getText());
+			System.out.println("Server listening at Port " + args[0]);
+
+			while(true){
+				System.out.println("Waiting for connection...");
+				Socket con = server.accept();
+				System.out.println("Client connected...");
+				Thread t = new ServerTest(con);
+				t.start();   
+			}
+								 
+		}catch (Exception e) {
+			e.printStackTrace();
+		} 
     } 
 } 
